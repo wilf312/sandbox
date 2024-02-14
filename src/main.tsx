3377ca0c9bde62063d744aa30ebf9b3efa0ffa5e
OkadaGenya <g.okada.wilf+github@gmail.com>
2024-02-14 13:26:09 +0900

    init

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Routes } from '@generouted/react-router';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Routes />
+    {/* <App /> */}
+  </React.StrictMode>
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>TODO: </h1>
+      <ul>
+        <li>
+          <a href="./sandbox/css">css</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
